150f29738a02135feadaa80ca562aecc3b9f8daf
umeshsolanki2005 <umeshsolanki2468@gmail.com>
2025-08-05 14:43:27 +0530

    error

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import AccountProvider from './context/AccountProvider';
 
 import Loader from './components/loader/Loader';
 
-const Messenger = lazy(() => import('./components/Messenger'));
+const Messenger = lazy(() => import ('./components/messenger'));
 
 function App() {
 
